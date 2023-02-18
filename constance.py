42490b3306ea222f6898d554b8d279597ecf3207
tris1702 <phuonghoand2001@gmail.com>
2023-02-18 15:08:09 +0700

    [MOCK_DATA]

--- constance.py
+++ constance.py
@@ -1,7 +1,111 @@
 class Constance:
     history = []
-    historyAV = []
-    historyCV = []
-    historyTV = []
-    historyA2V1 = []
+    historyAV = [
+        {
+            'ampe': 1,
+            'voltage': 10,
+            'time': '11:11:11'
+        },
+        {
+            'ampe': 2,
+            'voltage': 15,
+            'time': '11:11:11'
+        },
+        {
+            'ampe': 3,
+            'voltage': 22,
+            'time': '11:11:11'
+        },
+        {
+            'ampe': 5,
+            'voltage': 29,
+            'time': '11:11:11'
+        },
+        {
+            'ampe': 7,
+            'voltage': 30,
+            'time': '11:11:11'
+        }
+    ]
+    historyCV = [
+        {
+            'centimeter': 1,
+            'voltage': 10,
+            'time': '11:11:11'
+        },
+        {
+            'centimeter': 2,
+            'voltage': 15,
+            'time': '11:11:11'
+        },
+        {
+            'centimeter': 3,
+            'voltage': 22,
+            'time': '11:11:11'
+        },
+        {
+            'centimeter': 5,
+            'voltage': 29,
+            'time': '11:11:11'
+        },
+        {
+            'centimeter': 7,
+            'voltage': 30,
+            'time': '11:11:11'
+        }
+    ]
+    historyTV = [
+        {
+            'timepoint': 1,
+            'voltage': 10,
+            'time': '11:11:11'
+        },
+        {
+            'timepoint': 2,
+            'voltage': 15,
+            'time': '11:11:11'
+        },
+        {
+            'timepoint': 3,
+            'voltage': 22,
+            'time': '11:11:11'
+        },
+        {
+            'timepoint': 5,
+            'voltage': 29,
+            'time': '11:11:11'
+        },
+        {
+            'timepoint': 7,
+            'voltage': 30,
+            'time': '11:11:11'
+        }
+    ]
+    historyA2V1 = [
+        {
+            'ampe2': 1,
+            'voltage1': 10,
+            'time': '11:11:11'
+        },
+        {
+            'ampe2': 2,
+            'voltage1': 15,
+            'time': '11:11:11'
+        },
+        {
+            'ampe2': 3,
+            'voltage1': 22,
+            'time': '11:11:11'
+        },
+        {
+            'ampe2': 5,
+            'voltage1': 29,
+            'time': '11:11:11'
+        },
+        {
+            'ampe2': 7,
+            'voltage1': 30,
+            'time': '11:11:11'
+        }
+    ]
     root = None
